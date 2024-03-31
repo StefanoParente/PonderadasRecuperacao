a5ffb5e619c633200012990487058ad4ea00c34e
StefanoParente <110400227+StefanoParente@users.noreply.github.com>
2024-03-31 10:45:30 -0300

    Create piscapisca.ino

---
+++ piscapisca.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(8, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(8, HIGH); //ligado
+  delay(500);                     
+  digitalWrite(8, LOW);   //desligado
+  delay(200);                      
+}
+
+//link do tinkercad https://www.tinkercad.com/things/ehymkyXkNzL-ponderada-rec?sharecode=5waLv6HZbQYDOhiBfjc4U3U_a3gc7szPyyhChzrKlkI
